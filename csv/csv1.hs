f1ff88ceaa75355bd4efdb9fa9ba6728607acc34
Brody Berg <brodyberg@gmail.com>
2017-08-24 08:15:21 -0700

    Start learning Parsec

---
+++ csv/csv1.hs
@@ -0,0 +1,36 @@
+-- file: ch16/csv1.hs
+
+import Text.ParserCombinators.Parsec
+
+csvFile :: GenParser Char st [[String]]
+csvFile = 
+    do result <- many line
+        eof
+        return result
+
+line :: GenParser Char st [String]
+line = 
+    do result <- cells
+        eol
+        return result
+    
+cells :: GenParser Char st [String]
+cells = 
+    do first <- cellContent
+        next <- remainingCells
+        return (first : next)
+    
+remainingCells :: GenParser Char st [String]
+remainingCells =
+    (char ',' >> cells)
+    <|> (return [])
+
+cellContent :: GenParser Char String
+cellContent = 
+    many (noneOf ",\n")
+
+eol :: GenParser Char st Char
+eol = char '\n'
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unknown)" input
