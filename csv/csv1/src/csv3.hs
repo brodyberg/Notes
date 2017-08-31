01b300ae014fc83e1866edec10381214064d3887
Brody Berg <brodyberg@gmail.com>
2017-08-31 08:50:56 -0700

    More eol parsers

--- csv/csv1/src/csv3.hs
+++ csv/csv1/src/csv3.hs
@@ -9,6 +9,55 @@ eol = string "\n" <|> string "\n\r"
 eol2 :: GenParser Char st [Char]
 eol2 = string "\n\r" <|> string "\n"
 
+eol3 :: GenParser Char st Char
+eol3 = 
+    do char '\n'
+       char '\r' <|> return '\n'
+
+-- says: require an 'a'
+-- if you get a 'b' that's great, return 'b'
+-- if no 'b' though, return 'a'
+fooOption3 :: GenParser Char st Char
+fooOption3 = 
+    do char 'a'
+       char 'b' <|> return 'a'
+       
+-- first attempt at creating a function 
+-- taking a parser and a parameter and 
+-- then using it. doesn't compile
+-- I don't know how to work with contained
+-- types like GenParser
+-- myTest1 :: GenParser Char st Char -> Char
+-- myTest1 c =
+--     do char 'a'
+--        char c <|> return 'a' 
+
+-- how does do know we're working in the 
+-- ParsecT monad?
+-- How does one work with things like this
+-- and any other value as a parameter?
+-- myTest2 :: GenParser Char st Char -> Char
+-- myTest2 c =
+--     do char 'a'
+--        char 'b' <|> return 'a' 
+
+eol4 :: GenParser Char st Char
+eol4 = 
+    do option '\n' (char '\r')
+
+-- option is way different from above
+-- IFF we get an 'a' return 'a'
+-- IFF we get ANY OPTHER CHARACTER OTHER THAN 'b', return 'a'
+-- But if we get a 'b', return 'b'
+fooOption4 :: GenParser Char st Char
+fooOption4 =
+    do option 'a' (char 'b')
+    
+eol5 :: GenParser Char st [Char]
+eol5 =   try (string "\n\r")
+     <|> try (string "\r\n")
+     <|> string "\n"
+     <|> string "\r"
 
 -- how to call this thing?
 
