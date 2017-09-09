83874da7c62899b45cc127f93dfdcf0c862ec213
Brody Berg <brodyberg@gmail.com>
2017-09-09 09:57:17 -0700

    Namespace parser
    Prelude> :l parseBond.hs
    [1 of 1] Compiling YouCanDoThis     ( parseBond.hs, interpreted )
    Ok, modules loaded: YouCanDoThis.
    *YouCanDoThis> parse namespace "(unknown)" "foo.bar"
    Right ["foo","bar"]

--- csv/csv1/src/parseBond.hs
+++ csv/csv1/src/parseBond.hs
@@ -2,15 +2,38 @@ module YouCanDoThis where
 
 import Text.ParserCombinators.Parsec
 
--- bondFile :: GenParser Char st Char
--- bondFile = endBy namespace eol
+-- bondFile :: GenParser Char st String
+bondFile = endBy namespaceDeclaration eol
 
--- I want a thing that just handles "namespace"
-justNamespace :: GenParser Char st String
-justNamespace = 
+-- namespace :: GenParser Char st String
+namespaceDeclaration = 
     do 
-       namespace <- string "namespace"
-       return namespace
+       string "namespace"
+       char ' '
+--        return "foo"
+       c <- many anyChar
+       return c
+       --        return many (noneOf "\n")
+
+namespace = 
+    do 
+--       name <- sepBy string (char '.')    
+       name <- sepBy block (char '.')    
+       return name
+
+-- block :: GenParser Char st Char
+block :: GenParser Char st String
+block = 
+    do 
+        b <- many letter
+        return b
+
+-- -- I want a thing that just handles "namespace"
+-- justNamespace :: GenParser Char st String
+-- justNamespace = 
+--     do 
+--        namespace <- string "namespace"
+--        return namespace
 
 -- namespace :: GenParser Char st Char
 -- namespace = 
@@ -18,10 +41,10 @@ justNamespace =
 --        char ' '
 --        return many (noneOf ",\n\r\"")
 
--- eol :: GenParser Char st Char
--- eol = 
---     do char '\n'
---        char '\r' <|> return '\n'
+eol :: GenParser Char st Char
+eol = 
+    do char '\n'
+       char '\r' <|> return '\n'
        
 
 -- parseBondFile :: String -> Either ParseError [[String]]
