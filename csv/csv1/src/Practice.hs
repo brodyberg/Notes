3895f8f00608c3317adbf3331a289ecd9e3888eb
Brody Berg <brodyberg@gmail.com>
2017-10-25 08:29:45 -0700

    Haskell Book Chapter 2 work

---
+++ csv/csv1/src/Practice.hs
@@ -0,0 +1,19 @@
+-- module Practice where
+
+times3PlusY = x * 3 + y
+   where x = 3
+         y = 1000
+
+times5     = x * 5
+   where y = 10
+         x = 10 * 5 + y
+
+divideProblem = z / x + y
+   where x = 7
+         y = negate x
+         z = y * 10
+
+mult1      = x * y
+   where x = 5
+         y = 6
+
