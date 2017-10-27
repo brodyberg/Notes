d164bcd0bd1774c6a225a7f6a19045ee10445f33
Brody Berg <brodyberg@gmail.com>
2017-10-27 07:54:06 -0700

    Correct copy/paste column problem

--- HaskellBook/Reverse.hs
+++ HaskellBook/Reverse.hs
@@ -2,10 +2,10 @@ module Reverse where
 
 rvrs :: String -> String
 rvrs curry = third ++ " " ++ second ++ " " ++ first
-where 
-  first = take 5 curry
-  second = take 2 $ drop 6 curry
-  third = drop 9 curry
+  where 
+    first = take 5 curry
+    second = take 2 $ drop 6 curry
+    third = drop 9 curry
 
 main :: IO ()
 main = print $ rvrs "Curry is awesome"
