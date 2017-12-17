514ee34c4e955ed7eacc6ee0c285a3968055d91c
Brody Berg <brodyberg@gmail.com>
2017-12-17 12:23:32 -0800

    Start Chapter 10 Database exercise

---
+++ HaskellBook/DatabaseProcessing.hs
@@ -0,0 +1,42 @@
+module DatabaseProcessing where
+
+import Data.Time
+
+data DatabaseItem = DbString String
+                  | DbNumber Integer
+                  | DbDate   UTCTime
+                  deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase = 
+  [ DbDate (UTCTime 
+            (fromGregorian 1911 5 1)
+            (secondsToDiffTime 34123))
+  , DbNumber 9001
+  , DbString "Hello, World!"
+  , DbDate (UTCTime
+            (fromGregorian 1921 7 4)
+            (secondsToDiffTime 34123))]
+
+-- 1. filters for DbDate values and returns a 
+-- list of UTCTime values inside them
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = undefined
+
+-- 2. filters for DbNumber values and returns a
+-- list of Integer values inside them
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = undefined
+
+-- 3. Gets the most recent date
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = undefined
+
+-- 4. Sums all the DbNumber values
+sumDb :: [DatabaseItem] -> Integer
+sumDb = undefined
+
+-- 5. Gets average of the DbNumber values
+avgDb :: [DatabaseItem] -> Double
+avgDb = undefined
+                
