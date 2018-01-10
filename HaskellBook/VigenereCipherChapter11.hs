18d3817d20de6e54465a0af16db60c9c8d2dce4c
Brody Berg <brodyberg@gmail.com>
2018-01-10 09:11:57 -0800

    Progress on the Cipher

--- HaskellBook/VigenereCipherChapter11.hs
+++ HaskellBook/VigenereCipherChapter11.hs
@@ -1,7 +1,9 @@
 module VigenereCipher where
 
-import Data.Char (ord)
-
+import Data.Char (ord, chr)
+-- uhhh
+import Prelude
+--import GHC.List
 
 -- 1. Pick a keyword
 
@@ -24,18 +26,44 @@ import Data.Char (ord)
 --        -> ([Char], a, [a], [Char]) 
 --        -> ([Char], a, [a], [Char])
 rotate :: Char 
-       -> ([Char], Int, [Int], [Char]) 
-       -> ([Char], Int, [Int], [Char])
-rotate c (keyword, index, modAcc, acc) = 
-  (keyword, newIndex, shiftedOrd : modAcc, c : acc)
+       -> (String, Int, [Int], [Int], [Int], String) 
+       -> (String, Int, [Int], [Int], [Int], String)
+rotate c (keyword, index, modAcc, firstCodedOrds, finalCodedOrds, acc) = 
+  (keyword, newIndex, shiftedOrd : modAcc, firstCodedOrd : firstCodedOrds, finalCodedOrd : finalCodedOrds, encodedChar : acc)
   where 
     newIndex = index + 1
     shiftedOrd = ord $ keyword !! (mod index (length keyword))
+    wrapOrd n = n - (ord 'z' - ord 'a') - 1
+    charMod n
+      | n > ord 'z' = wrapOrd n
+      | otherwise   = n
+    firstCodedOrd = (ord c) + shiftedOrd
+    finalCodedOrd = charMod firstCodedOrd
+    encodedChar = chr finalCodedOrd -- chr . finalCodedOrd -- charMod firstCodedOrd-- $ (ord c) + shiftedOrd
 
 keyword = "brodybtest"
 message = "we will meet at dawn, that is the plan"
 
-(_, _, _, codeMessage) = foldr rotate (keyword, 0, [], "") message
+(_, ix, mods, firstCodedOrds, finalCodedOrds, codeMessage) = foldr rotate (keyword, 0, [], [], [], "") message
+
+-- this makes total sense given that the keyword is entirely 
+-- in a-z
+-- *VigenereCipher> any (\x -> x > (ord 'z') || x < (ord 'a')) mods
+-- False
+
+rangeOk :: [Int] -> Bool
+rangeOk = all (\x -> x > (ord 'z') || x < (ord 'a'))
+-- lol, 
+-- rangeOk $ map ord message
+-- True
+-- should be false: 32 is right in the list
+-- any isn't what I wanted, it was all
+
+-- now: 
+-- rangeOk $ map ord message
+-- False
+
+-- reversedMessage = revese codeMessage
 
 -- vigenere :: [Char] -> [Char] -> [Char]
 -- vigenere keyword = map mapper
