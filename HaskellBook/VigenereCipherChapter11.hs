6d2ddd053511ad3942d358b7c6ef4d6b4f830783
Brody Berg <brodyberg@gmail.com>
2018-01-09 17:14:08 -0800

    Vigenere WIP

--- HaskellBook/VigenereCipherChapter11.hs
+++ HaskellBook/VigenereCipherChapter11.hs
@@ -33,7 +33,27 @@ import Data.Char (ord)
 
 -- foldr (\c (index, modAcc, acc) -> (index + 1, (mod index (length keyword)) : modAcc, c : acc)) (0, [], "") message
 
-foldr (\c (index, modAcc, acc) -> (index + 1, (ord $ keyword !! (mod index (length keyword))) : modAcc, c : acc)) (0, [], "") message
+-- rotate :: Num a 
+--        => Char 
+--        -> ([Char], a, [a], [Char]) 
+--        -> ([Char], a, [a], [Char])
+rotate :: Char 
+       -> ([Char], Int, [Int], [Char]) 
+       -> ([Char], Int, [Int], [Char])
+rotate c (keyword, index, modAcc, acc) = 
+  (keyword, newIndex, shiftedOrd : modAcc, c : acc)
+  where 
+    newIndex = index + 1
+    shiftedOrd = ord $ keyword !! (mod index (length keyword))
+
+keyword = "brodybtest"
+message = "we will meet at dawn, that is the plan"
+
+result = foldr rotate (keyword, 0, [], "") message
+
+-- (index + 1, (ord $ keyword !! (mod index (length keyword))) : modAcc, c : acc)
+
+--foldr (\c (index, modAcc, acc) -> (index + 1, (ord $ keyword !! (mod index (length keyword))) : modAcc, c : acc)) (0, [], "") message
 
 -- map across message where we consume
 -- chars in the keyword in a loop
@@ -50,13 +70,13 @@ foldr (\c (index, modAcc, acc) -> (index + 1, (ord $ keyword !! (mod index (leng
 -- endlessZip :: [a] -> [b] -> [(a, b)]
 -- endlessZip []
 
-vigenere :: [Char] -> [Char] -> [Char]
-vigenere keyword = map mapper
-  where 
-    modShift  = fromIntegral $ mod shift 26
-    wrapOrd n = n - (ord 'z' - ord 'a') - 1
-    charMod n
-      | n > ord 'z' = wrapOrd n
-      | otherwise   = n
-    mapper ' ' = ' ' -- preserve spaces
-    mapper c = chr . charMod $ (ord c) + modShift
+-- vigenere :: [Char] -> [Char] -> [Char]
+-- vigenere keyword = map mapper
+--   where 
+--     modShift  = fromIntegral $ mod shift 26
+--     wrapOrd n = n - (ord 'z' - ord 'a') - 1
+--     charMod n
+--       | n > ord 'z' = wrapOrd n
+--       | otherwise   = n
+--     mapper ' ' = ' ' -- preserve spaces
+--     mapper c = chr . charMod $ (ord c) + modShift
