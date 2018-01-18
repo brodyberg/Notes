277609c7a3d072c8d4bf3dc4eb260116c58dd249
Brody Berg <brodyberg@gmail.com>
2018-01-18 08:30:22 -0800

    CapitalizeParagraph
    This turned out to be way harder than I thought for
    several reasons:
    
    1. When trying to build sentences it took a long
       time for me to realize the action of foldr was
       resulting in looking through characters from
       the end of the sentence rather than from the
       beginning. Once I realized this I reversed the
       incoming string - so the foldr processing could
       happen from the end and things worked as I'd
       expected.
    2. Still working on my skill of developing from
       tiny working kernels up. I wrote a whole program
       but it had the problem mentioned above and so
       I was forced to build all kinds of little examples
       in order to teach myself what was going on. From
       there I was able to systematically build the
       working solution part by part.

--- HaskellBook/LanguageExercisesChapter11.hs
+++ HaskellBook/LanguageExercisesChapter11.hs
@@ -2,109 +2,54 @@ module LanguageExercises where
 
 import Data.Char (toUpper)
 
+-- 1. Capitalize a word
+
 capitalizeWord :: String -> String
 capitalizeWord [] = []
 capitalizeWord (x:xs) = toUpper x : xs
 
 x = "the red fox jumped over the lazy cat. the green turtle slept."
 
--- isVowel :: Char -> Bool
--- isVowel 'a' = True
--- isVowel 'e' = True
--- isVowel 'i' = True
--- isVowel 'o' = True
--- isVowel 'u' = True
--- isVowel _   = False
+-- 2. Capitalize each sentence in a paragraph. 
+--    Re-use capitalizeWord
 
--- resultV = foldr (\c (vowels, acc) -> (if isVowel c then c : vowels else vowels, c : acc)) ("", "") x
-
--- resultL = 
---   foldr 
---     (\s (shorts, acc) -> 
---       (if length s <= 3 then s : shorts else shorts, s : acc)) 
---       ([], []) $ words x
-
-resultSeenOrNot = 
-  foldr 
-    (\c (untilSeen, seenList) -> 
-      let 
-        thisIsPeriod = c == '.' 
-        glom = c : untilSeen
-      in 
-        if thisIsPeriod
-        then ([], glom : seenList)
-        else (glom, seenList))
-    ([], [])
-    x
-    
-
--- resultSeenOrNot = 
---   foldr 
---     (\c (seen, untilSeen, seenList) -> 
---       let thisIsPeriod = c == '.' 
---       in 
---         if (thisIsPeriod == false)
---         then (seen, c : untilSeen, seenList)
---         else ()
-    
-    
---     )
-
-
--- fold
--- acc is (curr, [[String]])
--- if char is . then add it and curr to [[string]]
--- then
--- map each to capitalize first (watching for leading space)
--- then concat all
--- how to reuse capitalizeWord?
---   foldr (++) $ capitalizeWord $ head $ words sentence?
-
-sentences :: [Char] -> [[Char]]
-sentences s = result
-  where (_, result) = foldr adder ("", []) s
-
-sentences' :: [Char] -> ([Char], [[Char]])
-sentences' s = (curr, result)
-  where (curr, result) = foldr adder ("", []) s
-
-sentences'' :: [Char] -> ([Char], [[Char]])
-sentences'' s = (curr, result)
-  where (curr, result) = foldr adder_ ("", []) s
-  
-adder_ :: Char
-  -> ([Char], [[Char]])
-  -> ([Char], [[Char]])
-adder_ '.' (curr, sents) = ("", ('.' : curr) : sents)
-adder_ c   (curr, sents) = (c : curr, sents)
-
-adder :: Char
-      -> ([Char], [[Char]])
-      -> ([Char], [[Char]])
-adder c (curr, sents) = 
-  let added = c : curr
-  in 
-    if c == '.' 
-    then (added,    added : sents) 
-    else (added, sents)
-
-capitalizedSentence :: [Char] -> [Char]
-capitalizedSentence [] = []
-capitalizedSentence s = firstCapitalized ++ " " ++ (foldr (++) "" listRest)
-  where
-    listWords :: [[Char]]
-    listWords = words s
-    firstWord ::[Char]
-    firstWord = head listWords
-    listRest :: [[Char]]
-    listRest = tail listWords
-    firstCapitalized = capitalizeWord firstWord
-
-capitalizedSentences :: [Char] -> [[Char]]
-capitalizedSentences s = map capitalizedSentence $ sentences s
-
-capitalizeParagraph :: [Char] -> [Char] 
+capitalizeParagraph :: String -> String
 capitalizeParagraph [] = []
-capitalizeParagraph s = 
-  foldr (++) "" $ capitalizedSentences s
-
+capitalizeParagraph p = capitalizedParagraph
+  where 
+    (_, reversedSentences) = 
+      foldr 
+        (\c (untilSeen, seenList) -> 
+          let 
+            thisIsPeriod = c == '.' 
+            glom = c : untilSeen
+          in 
+            if thisIsPeriod
+            then ([], glom : seenList)
+            else (glom, seenList))
+        ([], [])
+        $ reverse p
+    nonEmptyNonReversedSentences = 
+      foldr 
+        (\s acc -> 
+          if (length s) > 0 
+          then (reverse s) : acc 
+          else acc) 
+        []
+        reversedSentences
+    capitalizedSentences = 
+      foldr 
+        (\s acc -> 
+          let 
+            allTheWords = words s
+            firstWord   = head allTheWords
+            otherWords  = tail allTheWords
+            firstCapd   = capitalizeWord firstWord
+            rawOutput   = firstCapd : otherWords
+            cappedAndJoined = foldr (\w wacc -> w ++ " " ++ wacc) "" rawOutput
+          in 
+            cappedAndJoined : acc)
+        []
+        nonEmptyNonReversedSentences
+    capitalizedParagraph = 
+      foldr (++) "" $ reverse capitalizedSentences
