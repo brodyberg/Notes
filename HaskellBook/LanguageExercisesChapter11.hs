b76cf3633b7888632723607e46f44b86f4be4853
Brody Berg <brodyberg@gmail.com>
2018-01-19 08:27:55 -0800

    Rework things

--- HaskellBook/LanguageExercisesChapter11.hs
+++ HaskellBook/LanguageExercisesChapter11.hs
@@ -13,43 +13,43 @@ x = "the red fox jumped over the lazy cat. the green turtle slept."
 -- 2. Capitalize each sentence in a paragraph. 
 --    Re-use capitalizeWord
 
+withSpaces :: [[Char]] -> [Char]
+withSpaces = foldr (\w wacc -> w ++ " " ++ wacc) ""
+
 capitalizeParagraph :: String -> String
-capitalizeParagraph [] = []
-capitalizeParagraph p = capitalizedParagraph
+capitalizeParagraph = 
+  capitalizedSentences . 
+  nonEmptyNonReversedSentences . 
+  makeReversedSentences
   where 
-    (_, reversedSentences) = 
-      foldr 
-        (\c (untilSeen, seenList) -> 
-          let 
-            thisIsPeriod = c == '.' 
-            glom = c : untilSeen
-          in 
-            if thisIsPeriod
-            then ([], glom : seenList)
-            else (glom, seenList))
-        ([], [])
-        $ reverse p
-    nonEmptyNonReversedSentences = 
+    makeReversedSentences paragraph = reversedSentences 
+      where
+        (_, reversedSentences) = 
+          foldr 
+            (\c (untilSeen, seenList) -> 
+              let 
+                thisIsPeriod = c == '.' 
+                glom = c : untilSeen
+              in 
+                if thisIsPeriod
+                then ([], glom : seenList)
+                else (glom, seenList))
+            ([], [])
+            $ reverse paragraph
+    nonEmptyNonReversedSentences paragraph = 
       foldr 
         (\s acc -> 
-          if (length s) > 0 
+          if length s > 0 
           then (reverse s) : acc 
           else acc) 
         []
-        reversedSentences
-    capitalizedSentences = 
+        $ makeReversedSentences paragraph
+    capitalizedSentences paragraph = 
       foldr 
         (\s acc -> 
           let 
-            allTheWords = words s
-            firstWord   = head allTheWords
-            otherWords  = tail allTheWords
-            firstCapd   = capitalizeWord firstWord
-            rawOutput   = firstCapd : otherWords
-            cappedAndJoined = foldr (\w wacc -> w ++ " " ++ wacc) "" rawOutput
+            (firstWord : otherwords) = words s
           in 
-            cappedAndJoined : acc)
+            acc ++ (withSpaces $ (capitalizeWord firstWord) : otherwords))
         []
-        nonEmptyNonReversedSentences
-    capitalizedParagraph = 
-      foldr (++) "" $ reverse capitalizedSentences
+        $ nonEmptyNonReversedSentences paragraph
