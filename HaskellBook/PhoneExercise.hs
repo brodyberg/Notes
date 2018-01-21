d5eb09d30b226b3c72e54a636d0ddcc93c0b7a0f
Brody Berg <brodyberg@gmail.com>
2018-01-21 14:20:51 -0800

    Try the type design again
    Even though this feels less safe it feels way easier
    to use and even my previous iteration which felt "stronger"
    looked like there were still loopholes which allowed
    incorrectness and to fix would make it even harder
    to use. So rather than go down that road I chose something
    simpler that would at least be usable.

--- HaskellBook/PhoneExercise.hs
+++ HaskellBook/PhoneExercise.hs
@@ -3,104 +3,161 @@ module Phone where
 -- 1. Create a datastructure that captures the 
 --    phone layout on page 457. 
 
-data Value = 
-    One   Char 
-  | Two   Char Char 
-  | Three Char Char Char
-  | Four  Char Char Char Char
-  deriving (Eq, Show)
-
-toString :: Value -> String
-toString (One c) = [c]
-toString (Two c r) = [c, r]
-toString (Three c r e) = [c, r, e]
-toString (Four c r e w) = [c, r, e, w]
+-- Char to Key
+-- Key to Char
 
 data Key = 
     Num Int
-  | NumAlpha Int Value
-  | Alpha Int Value
-  | Symbol Char Value
+  | Alpha Char
   deriving (Eq, Show)
 
-keyToValidValues :: Key -> String
-keyToValidValues (Num x)        = show x
-keyToValidValues (NumAlpha x v) = show x ++ toString v
-keyToValidValues (Alpha x v)    = show x ++ toString v
-keyToValidValues (Symbol s v)   = [s] ++ toString v
+data Value = Value [Char]
+  deriving (Eq, Show)
 
-keyRowToValidValues :: KeyRow -> String
-keyRowToValidValues (KeyRow x y z) = 
-  foldr (++) "" $ map keyToValidValues [x, y, z]
+data Phone = Phone [Key] [Value]
+  deriving (Eq, Show)
 
-keyToValidButtons :: Key -> String
-keyToValidButtons (Num x)        = show x
-keyToValidButtons (NumAlpha x _) = show x
-keyToValidButtons (Alpha x _)    = show x
-keyToValidButtons (Symbol s _)   = [s]
+aPhone = Phone [(Num 1),     (Num 2),  (Num 3),
+                (Num 4),     (Num 5),  (Num 6),
+                (Num 7),     (Num 8),  (Num 9),
+                (Alpha '*'), (Num 0),  (Alpha '#')]
 
-keyRowToValidButtons :: KeyRow -> String
-keyRowToValidButtons (KeyRow x y z) = 
-  foldr (++) "" $ map keyToValidButtons [x, y, z]
+               [(Value []),                   (Value ['a','b','c']),   (Value ['d', 'e', 'f']),
+                (Value ['g', 'h', 'i']),      (Value ['j', 'k', 'l']), (Value ['m', 'n', 'o']),
+                (Value ['p', 'q', 'r', 's']), (Value ['t', 'u', 'v']), (Value ['w', 'x', 'y', 'z']),
+                (Value ['^']),                (Value ['+', '_']),      (Value ['.', ','])]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- data Value = 
+--     One   Char 
+--   | Two   Char Char 
+--   | Three Char Char Char
+--   | Four  Char Char Char Char
+--   deriving (Eq, Show)
+
+-- toString :: Value -> String
+-- toString (One c) = [c]
+-- toString (Two c r) = [c, r]
+-- toString (Three c r e) = [c, r, e]
+-- toString (Four c r e w) = [c, r, e, w]
+
+-- data Key = 
+--     Num Int
+--   | NumAlpha Int Value
+--   | Alpha Int Value
+--   | Symbol Char Value
+--   deriving (Eq, Show)
+
+-- keyToValidValues :: Key -> String
+-- keyToValidValues (Num x)        = show x
+-- keyToValidValues (NumAlpha x v) = show x ++ toString v
+-- keyToValidValues (Alpha x v)    = show x ++ toString v
+-- keyToValidValues (Symbol s v)   = [s] ++ toString v
+
+-- keyRowToValidValues :: KeyRow -> String
+-- keyRowToValidValues (KeyRow x y z) = 
+--   foldr (++) "" $ map keyToValidValues [x, y, z]
+
+-- keyToValidButtons :: Key -> String
+-- keyToValidButtons (Num x)        = show x
+-- keyToValidButtons (NumAlpha x _) = show x
+-- keyToValidButtons (Alpha x _)    = show x
+-- keyToValidButtons (Symbol s _)   = [s]
+
+-- keyRowToValidButtons :: KeyRow -> String
+-- keyRowToValidButtons (KeyRow x y z) = 
+--   foldr (++) "" $ map keyToValidButtons [x, y, z]
   
-data KeyRow = KeyRow Key Key Key
-  deriving (Eq, Show)
+-- data KeyRow = KeyRow Key Key Key
+--   deriving (Eq, Show)
 
-data Phone = Phone KeyRow KeyRow KeyRow KeyRow
-  deriving (Eq, Show)
+-- data Phone = Phone KeyRow KeyRow KeyRow KeyRow
+--   deriving (Eq, Show)
 
-class KeyPad a where
-  validButtons :: a -> String
-  validValues  :: a -> String
+-- class KeyPad a where
+--   validButtons :: a -> String
+--   validValues  :: a -> String
+--   charToKey    :: a -> Char -> Key
 
-instance KeyPad Phone where
-  validButtons (Phone kr1 kr2 kr3 kr4) = 
-    foldr (++) "" $ map keyRowToValidButtons [kr1, kr2, kr3, kr4]
-  validValues (Phone kr1 kr2 kr3 kr4) = 
-    foldr (++) "" $ map keyRowToValidValues [kr1, kr2, kr3, kr4]
+-- instance KeyPad Phone where
+--   validButtons (Phone kr1 kr2 kr3 kr4) = 
+--     foldr (++) "" $ map keyRowToValidButtons [kr1, kr2, kr3, kr4]
+--   validValues (Phone kr1 kr2 kr3 kr4) = 
+--     foldr (++) "" $ map keyRowToValidValues [kr1, kr2, kr3, kr4]
+--   -- charToKey (Phone (KeyRow r1k1 r1k2 r1k3) (KeyRow  kr4) c = 
+--   --   filter (\() -> ) [kr1, kr2, kr3, kr4]
+--   charToKey = undefined
 
-thisPhone :: Phone
-thisPhone = 
-  Phone 
-    (KeyRow 
-      (Num 1)                          (Alpha 2 (Three 'A' 'B' 'C')) (Alpha 3 (Three 'D' 'E' 'F')))
-    (KeyRow
-      (Alpha 4 (Three 'G' 'H' 'I'))    (Alpha 5 (Three 'J' 'K' 'L')) (Alpha 6 (Three 'M' 'N' 'O')))
-    (KeyRow
-      (Alpha 7 (Four 'P' 'Q' 'R' 'S')) (Alpha 8 (Three 'T' 'U' 'V')) (Alpha 9 (Four 'W' 'X' 'Y' 'Z')))
-    (KeyRow
-      (Symbol '*' (One '^'))           (NumAlpha 0 (Two '+' '_'))     (Symbol '#' (Two '.' ',')))
+-- -- you'll get Char
+-- -- you'll need to look up the number for that Char
+-- -- 
 
--- 2. Convert the following conversations into the keypresses
---    required to express them. 
+-- thisPhone :: Phone
+-- thisPhone = 
+--   Phone 
+--     (KeyRow 
+--       (Num 1)                          (Alpha 2 (Three 'A' 'B' 'C')) (Alpha 3 (Three 'D' 'E' 'F')))
+--     (KeyRow
+--       (Alpha 4 (Three 'G' 'H' 'I'))    (Alpha 5 (Three 'J' 'K' 'L')) (Alpha 6 (Three 'M' 'N' 'O')))
+--     (KeyRow
+--       (Alpha 7 (Four 'P' 'Q' 'R' 'S')) (Alpha 8 (Three 'T' 'U' 'V')) (Alpha 9 (Four 'W' 'X' 'Y' 'Z')))
+--     (KeyRow
+--       (Symbol '*' (One '^'))           (NumAlpha 0 (Two '+' '_'))    (Symbol '#' (Two '.' ',')))
 
-validButtons' = validButtons thisPhone
-validValues'  = validValues  thisPhone
+-- -- 2. Convert the following conversations into the keypresses
+-- --    required to express them. 
 
-convo :: [String]
-convo = [
-  "Hope is the beginning of despair",
-  "Ok Marneas calm down",
-  "We were betrayed at Calth",
-  "True enough",
-  "Only the Emperor can save us now",
-  "Since when do Ultramarines say that",
-  "Put on your fancy gauntlets and fight"]
+-- validButtons' = validButtons thisPhone
+-- validValues'  = validValues  thisPhone
 
-type Digit = Char
-type Presses = Int
+-- convo :: [String]
+-- convo = [
+--   "Hope is the beginning of despair",
+--   "Ok Marneas calm down",
+--   "We were betrayed at Calth",
+--   "True enough",
+--   "Only the Emperor can save us now",
+--   "Since when do Ultramarines say that",
+--   "Put on your fancy gauntlets and fight"]
 
-reverseTaps :: Phone
-            -> Char 
-            -> [(Digit, Presses)]
-reverseTaps = undefined
+-- type Digit = Char
+-- type Presses = Int
 
--- assuming the default phone definition
--- 'a' -> [('2', 1)]
--- 'A' -> [('*', 1), ('2', 1)]
+-- charToTaps :: Phone
+--             -> Char 
+--             -> [(Digit, Presses)]
+-- charToTaps p c = undefined
+--   -- look through each row
+--   -- for each key
+--   -- in the possible values
+--   -- look for c
+--   -- return taps aka ordinal of letter or (length + 1) for #
 
-cellPhonesDead :: Phone
-               -> String
-               -> [(Digit, Presses)]
-cellPhonesDead = undefined
+
+
+-- -- assuming the default phone definition
+-- -- 'a' -> [('2', 1)]
+-- -- 'A' -> [('*', 1), ('2', 1)]
+
+-- -- charToTaps for first sentence: 
+
+-- -- [('*', 1), (2, 4), (3, 6)]
+
+-- cellPhonesDead :: Phone
+--                -> String
+--                -> [(Digit, Presses)]
+-- cellPhonesDead = undefined
 
