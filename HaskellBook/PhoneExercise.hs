a162a2041fecc918692aeeaded829630cefcf9b3
Brody Berg <brodyberg@gmail.com>
2018-01-21 20:44:59 -0800

    Figured out basic reverse translation
    But also painfully that this is not legal:
    
    let
      (key, values) = tupleThingy
    in
      ...
    
    Causes a super strange error

--- HaskellBook/PhoneExercise.hs
+++ HaskellBook/PhoneExercise.hs
@@ -4,43 +4,6 @@ import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 import Data.Char (isUpper, toLower)
 
--- 1. Create a datastructure that captures the 
---    phone layout on page 457. 
-
-data Phone = Phone String [String]
-  deriving (Eq, Show)
-
-standardPhone :: Phone
-standardPhone
-       = Phone "123456789*0#"
-               ["",     "abc", "def", 
-                "ghi",  "jkl", "mno",
-                "pqrs", "tuv", "wxyz", 
-                "^",    "+ ",  ".,"]
-
-standardCharToTaps :: Char 
-                   -> [(Digit, Presses)]
-standardCharToTaps = charToTaps standardPhone
-
-charToTaps :: Phone
-           -> Char 
-           -> [(Digit, Presses)]
-charToTaps (Phone keys values) c = 
-  if isUpper c 
-  then ('*', 1) : (charToTaps (Phone keys values) $ toLower c)
-  else thePress
-  where
-    f (k, v) acc = 
-      if elem c (v ++ [k]) 
-      then (k, pressCount c v): acc 
-      else acc
-    pressCount :: Char -> String -> Presses
-    pressCount k v = (fromJust $ elemIndex c (v ++ [k])) + 1
-    thePress = foldr f [] $ zip keys values
-    
-(Phone keys values) = standardPhone
-kv = zip keys values
-
 convo :: [String]
 convo = [
   "Hope is the beginning of despair",
@@ -58,7 +21,79 @@ type Presses = Int
 -- 'a' -> [('2', 1)]
 -- 'A' -> [('*', 1), ('2', 1)]
 
-cellPhonesDead :: Phone
-               -> String
-               -> [(Digit, Presses)]
-cellPhonesDead = undefined
+-- 1. Create a datastructure that captures the
+--    phone layout on page 457.
+
+data Phone = Phone String [String]
+  deriving (Eq, Show)
+
+standardPhone :: Phone
+standardPhone
+       = Phone "123456789*0#"
+               ["",     "abc", "def",
+                "ghi",  "jkl", "mno",
+                "pqrs", "tuv", "wxyz",
+                "^",    "+ ",  ".,"]
+
+standardCharToTaps :: Char
+                   -> [(Digit, Presses)]
+standardCharToTaps = charToTaps standardPhone
+
+charToTaps :: Phone
+           -> Char
+           -> [(Digit, Presses)]
+charToTaps (Phone keys values) c =
+  if isUpper c
+  then ('*', 1) : (charToTaps (Phone keys values) $ toLower c)
+  else thePress
+  where
+    f (k, v) acc =
+      if elem c (v ++ [k])
+      then (k, pressCount c v): acc
+      else acc
+    pressCount :: Char -> String -> Presses
+    pressCount k v = (fromJust $ elemIndex c (v ++ [k])) + 1
+    thePress = foldr f [] $ zip keys values
+
+
+tapsToChar :: Phone
+           -> [(Digit, Presses)]
+--           -> [(Digit, String)]
+           -> Char
+tapsToChar (Phone keys values) taps =
+  let
+    (digit, presses) = taps !! 0
+    kv = zip keys values
+    filtered = filter (\(c, str) -> c == digit) kv
+    -- (key, values) = head filtered
+    x = head filtered
+    makeTheStr (key, values) = 
+      values ++ [key]
+
+    -- values = "abc"
+    -- key = '2'
+--    theStr = values ++ [key]
+  in
+    (makeTheStr x) !! (presses - 1)
+    --theStr !! (mod presses (length theStr)) -- mod by length
+    --'d'
+    --filtered
+
+tapsa = [('2', 1)]
+tapsA = [('*', 1), ('2', 1)]
+
+
+(Phone keys values) = standardPhone
+kv = zip keys values
+
+standardStringToTaps :: String
+                     -> [(Digit, Presses)]
+standardStringToTaps = stringToTaps standardPhone
+
+stringToTaps :: Phone
+             -> String
+             -> [(Digit, Presses)]
+stringToTaps p s = foldr (++) [] $ map (\c -> charToTaps p c) s
+-- standardStringToTaps $ convo !! 1
+-- [('*',1),('6',3),('5',2),('0',2),('*',1),('6',1),('2',1),('7',3),('6',2),('3',2),('2',1),('7',4),('0',2),('2',3),('2',1),('5',3),('6',1),('0',2),('3',1),('6',3),('9',1),('6',2)]
+
