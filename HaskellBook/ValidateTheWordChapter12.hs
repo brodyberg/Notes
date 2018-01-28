2b7f93f1af2a9d3b456f48076d8ead168b0fb8a3
Brody Berg <brodyberg@gmail.com>
2018-01-28 13:26:17 -0800

    Implement ValidateTheWord

--- HaskellBook/ValidateTheWordChapter12.hs
+++ HaskellBook/ValidateTheWordChapter12.hs
@@ -1,5 +1,8 @@
 module ValidateTheWord where
 
+import Data.List (elemIndex)
+
+
 -- Use the Maybe type to write a function that 
 -- counts the number of vowels in a string and the
 -- number of consonants. If the number of vowels
@@ -12,9 +15,26 @@ newtype Word' =
   Word' String 
   deriving (Eq, Show)
 
-vowels = "aeiou"
+countVowels :: String -> Int
+countVowels s = 
+  foldr (\c acc -> if isVowel c then 1 + acc else acc) 0 s 
+  where
+    isVowel c =    
+      case elemIndex c "aeoiu" of
+        Just _ -> True
+        _      -> False
 
 mkWord :: String -> Maybe Word' 
-mkWord = undefined
+mkWord w = 
+  if vowelCount > consonantCount
+  then Nothing
+  else Just (Word' w)
+  where
+    vowelCount = countVowels w
+    consonantCount = length w - vowelCount
+
+
+
+
 
 -- Next: ItsOnlyNaturalChapter12.hs
