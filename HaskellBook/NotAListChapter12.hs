dd7e16ca456e6e2b1ea822578d4d49c68e57749e
Brody Berg <brodyberg@gmail.com>
2018-02-01 07:38:00 -0800

    Breakthrough, but not done
    This problem requires the function passed to unfold to
    somehow "produce" types a and b. It took me a long time
    to realize that I had control of constraining those
    types - and thus explicitly controlling the type information
    that flowed back to unfold. Otherwise I had been stuck
    trying to produce a 'b' value from thin air.
    
    The 'not done' part at this point is that my tree counts
    down while their tree counts up toward the leaves.

--- HaskellBook/NotAListChapter12.hs
+++ HaskellBook/NotAListChapter12.hs
@@ -10,21 +10,32 @@ data BinaryTree a =
 
 -- 1. Write unfold for BinaryTree
 
+-- the secret here is that *I* define what is sent
+-- into unfold, so I can specify a and b to what I want
+-- for the BinaryTree
+
+maker :: Int
+      -> Maybe(Int, Int, Int)
+maker 0 = Nothing
+maker x = Just(x - 1, x - 1, x - 1) 
+
 unfold :: (a -> Maybe (a, b, a))
        -> a
        -> BinaryTree b
-unfold _ 0 = Leaf
-unfold f n = Node (unfold f (n - 1)) n (unfold f (n - 1))
+unfold f n = 
+  case f n of
+    Just(x, y, z) -> (Node (unfold f x) y (unfold f z))
+    Nothing       -> Leaf
 
--- 2. Maybe a tree builder. 
+-- 2. Make a tree builder. 
 --    Using the unfold function you've made for 
 --    BinaryTree, write the following function: 
 
-treeBuild :: Integer -> BinaryTree Integer
-treeBuild = undefined
+treeBuild :: Int -> BinaryTree Int
+treeBuild = unfold maker 
 
 -- treeBuild 0
--- Lead
+-- Leaf
 
 -- treeBuild 1
 -- Node Leaf 0 Leaf
