5947de4e2b3e4921c9f163712d9ffaba5a2a4ec7
Brody Berg <brodyberg@gmail.com>
2018-03-20 07:58:11 -0700

    MorseCode work

--- MorseCode/src/Main.hs
+++ MorseCode/src/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Control.Monad (forever, when)
-import Data.List (intercalate)
-import Data.Traversable (traverse)
-import Morse (stringToMorse, morseToChar)
+import Control.Monad      (forever, when)
+import Data.List          (intercalate)
+import Data.Traversable   (traverse)
+import Morse              (stringToMorse, morseToChar)
 import System.Environment (getArgs)
-import System.Exit (exitFailure, exitSuccess)
-import System.IO (hGetLine, hIsEOF, stdin)
+import System.Exit        (exitFailure, exitSuccess)
+import System.IO          (hGetLine, hIsEOF, stdin)
 
 convertToMorse :: IO ()
 convertToMorse = forever $ do
