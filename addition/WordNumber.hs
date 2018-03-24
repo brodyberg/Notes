a73e48ec469985374d605b274459d64995a42471
Brody Berg <brodyberg@gmail.com>
2018-03-24 15:22:54 -0700

    Simplify and correct digitToWord

--- addition/WordNumber.hs
+++ addition/WordNumber.hs
@@ -5,28 +5,18 @@ import Data.Maybe
 
 digitToWord :: Int -> Maybe String
 digitToWord n = 
-  if n < 0 || n > 9 
-  then Nothing
-  else Just (translate n)
-  where 
-    translate :: Int -> String
-    translate x = 
-      case x of
-      0 -> "zero"
-      1 -> "one"
-      2 -> "two"
-      3 -> "three"
-      4 -> "four"
-      5 -> "five"
-      6 -> "six"
-      7 -> "seven"
-      8 -> "eight"
-      9 -> "nine"
-
--- *WordNumber> reverse $ (542 `mod` 10) : ((542 `quot` 10) `mod` 10) : (((542 `quot` 10) `quot` 10) `mod` 10) : []
--- [5,4,2]
--- *WordNumber> reverse $ (5421 `mod` 10) : ((5421 `quot` 10) `mod` 10) : (((5421 `quot` 10) `quot` 10) `mod` 10) : ((((5421 `quot` 10) `quot` 10) `quot` 10) `mod` 10) : []
--- [5,4,2,1]
+  case n of
+    0 -> Just "zero"
+    1 -> Just "one"
+    2 -> Just "two"
+    3 -> Just "three"
+    4 -> Just "four"
+    5 -> Just "five"
+    6 -> Just "six"
+    7 -> Just "seven"
+    8 -> Just "eight"
+    9 -> Just "nine"
+    _ -> Nothing
 
 digits :: Int -> [Int]
 digits n = 
