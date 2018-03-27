867cfa88a1dabf41983a439c3f015f3280da14ac
Brody Berg <brodyberg@gmail.com>
2018-03-27 08:45:55 -0700

    QuickCheck for list ordering

--- addition/Chapter14Exercises.hs
+++ addition/Chapter14Exercises.hs
@@ -1,6 +1,5 @@
 module Chapter14Exercises where
 
-import Test.Hspec
 import Test.QuickCheck
 
 half :: Fractional a => a -> a 
@@ -10,8 +9,4 @@ halfIdentity :: (Eq a, Fractional a) => a -> Bool
 halfIdentity x = (((*2) . half) x) == x
 
 main :: IO ()
-main = 
-  hspec $ do 
-    describe "half" $ do
-      it "Number *2 after half is Number" $ do
-        property (halfIdentity :: Double -> Bool)
+main = verboseCheck (halfIdentity :: Double -> Bool)
