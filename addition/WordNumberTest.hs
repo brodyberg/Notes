8e1fbe45fd290e7f370a42e47acbbbbd5e992c0c
Brody Berg <brodyberg@gmail.com>
2018-03-27 07:36:03 -0700

    WordNumberTests with all the tests, no warnings
    Run the tests in ghci with: main
    
    There isn't a test that runs through non-negative
    ints but I think that level of sophistication will
    be something we can get to in the future.

--- addition/WordNumberTest.hs
+++ addition/WordNumberTest.hs
@@ -2,10 +2,8 @@ module WordNumberTest where
 
 import Test.Hspec
 import Test.QuickCheck
-import Test.QuickCheck.Modifiers
---import Test.QuickCheck.Property-- (succeeded, failed)-
+-- import Test.QuickCheck.Modifiers
 import WordNumber 
---  (digitToWord, wordToDigit, digits, numbersToBase10, numberWord, wordNumber)
   (digitToWord, digits, numberWord, wordNumber)
 
 zeroThroughNineDigit :: Gen Int
@@ -23,10 +21,10 @@ zeroThroughNineWord =
 
 -- prop_all_numberWordNumber :: Property
 -- prop_all_numberWordNumber = 
-prop_numberWordNumber :: Property
-prop_numberWordNumber = 
-  forAll zeroThroughNineDigit
-  (\c -> (numberWord $ wordNumber c) == c)
+-- prop_numberWordNumber :: Property
+-- prop_numberWordNumber = 
+--   forAll zeroThroughNineDigit
+--   (\c -> (numberWord $ wordNumber c) == c)
   
 prop_numberWordNumber :: Property
 prop_numberWordNumber = 
@@ -42,51 +40,51 @@ main :: IO ()
 --main = quickCheck prop_wordNumberWord
 -- main = verboseCheck prop_wordNumberWord
 -- main = verboseCheck prop_numberWordNumber
-
--- hspec $ do 
---   describe "digitToWord" $ do
---     it "returns Nothing for -1" $ do
---       digitToWord (-1) `shouldBe` Nothing
---     it "returns Nothing for 10" $ do
---       digitToWord 10 `shouldBe` Nothing
---     it "returns Just \"zero\" for 0" $ do
---       digitToWord 0 `shouldBe` (Just "zero")
---     it "returns Just \"one\" for 1" $ do
---       digitToWord 1 `shouldBe` (Just "one")
---     it "returns Just \"nine\" for 9" $ do
---       digitToWord 9 `shouldBe` (Just "nine")
+main = 
+  hspec $ do 
+    describe "digitToWord" $ do
+      it "returns Nothing for -1" $ do
+        digitToWord (-1) `shouldBe` Nothing
+      it "returns Nothing for 10" $ do
+        digitToWord 10 `shouldBe` Nothing
+      it "returns Just \"zero\" for 0" $ do
+        digitToWord 0 `shouldBe` (Just "zero")
+      it "returns Just \"one\" for 1" $ do
+        digitToWord 1 `shouldBe` (Just "one")
+      it "returns Just \"nine\" for 9" $ do
+        digitToWord 9 `shouldBe` (Just "nine")
+        
+    describe "digits" $ do
+      it "returns [] for -1" $ do
+        digits (-1) `shouldBe` [] 
+      it "returns [0] for 0" $ do
+        digits 0 `shouldBe` [0]
+      it "returns [1] for 1" $ do
+        digits 1 `shouldBe` [1]
+      it "returns [0,1] for 10" $ do
+        digits 10 `shouldBe` [0,1]
+      it "returns [4,3,2,1] for 1234" $ do
+        digits 1234 `shouldBe` [4,3,2,1]
       
---   describe "digits" $ do
---     it "returns [] for -1" $ do
---       digits (-1) `shouldBe` [] 
---     it "returns [0] for 0" $ do
---       digits 0 `shouldBe` [0]
---     it "returns [1] for 1" $ do
---       digits 1 `shouldBe` [1]
---     it "returns [0,1] for 10" $ do
---       digits 10 `shouldBe` [0,1]
---     it "returns [4,3,2,1] for 1234" $ do
---       digits 1234 `shouldBe` [4,3,2,1]
-    
---   describe "wordNumber" $ do
---     it "returns \"\" for -1" $ do
---       wordNumber (-1) `shouldBe` ""
---     it "returns zero for 0" $ do
---       wordNumber 0 `shouldBe` "zero"
---     it "returns \"one zero\" for 10" $ do
---       wordNumber 10 `shouldBe` "one zero"
---     it "returns \"one two three four\" for 1234" $ do
---       wordNumber 1234 `shouldBe` "one two three four"
---     -- it "x: try to do quickcheck" $ do
---     --   property prop_x
---     it "prop: prop_numberWordNumber" $ do
---       property prop_numberWordNumber
---     it "prop: prop_wordNumberWord" $ do
---       property prop_wordNumberWord
-    -- it "y: try to do quickcheck" $ do
-    --   property prop_y
-    -- it "z: try to do quickcheck" $ do
-    --   property prop_z
+    describe "wordNumber" $ do
+      it "returns \"\" for -1" $ do
+        wordNumber (-1) `shouldBe` ""
+      it "returns zero for 0" $ do
+        wordNumber 0 `shouldBe` "zero"
+      it "returns \"one zero\" for 10" $ do
+        wordNumber 10 `shouldBe` "one zero"
+      it "returns \"one two three four\" for 1234" $ do
+        wordNumber 1234 `shouldBe` "one two three four"
+      -- it "x: try to do quickcheck" $ do
+      --   property prop_x
+      it "prop: prop_numberWordNumber" $ do
+        property prop_numberWordNumber
+      it "prop: prop_wordNumberWord" $ do
+        property prop_wordNumberWord
+      -- it "y: try to do quickcheck" $ do
+      --   property prop_y
+      -- it "z: try to do quickcheck" $ do
+      --   property prop_z
 
 
       -- it "can round-trip using number -> word -> number" $ do
