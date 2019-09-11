bd52f4a55bc53f91543066f59c2688e127ecefe4
Brody Berg <brodyberg@gmail.com>
2019-09-10 21:41:08 -0700

    requireAlphaNum

--- FindingSuccessAndFailure/validation-book/src/Main.hs
+++ FindingSuccessAndFailure/validation-book/src/Main.hs
@@ -1,9 +1,17 @@
 module Main where
 
+import Data.Char
+
 checkPasswordLength :: String -> Maybe String
 checkPasswordLength password = 
   case (length password < 20) of
     True -> Just password
+    _ -> Nothing
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum xs = 
+  case (all isAlphaNum xs) of 
+    True -> Just xs
     _ -> Nothing
 
 main :: IO ()
@@ -11,4 +19,5 @@ main =
   do
     putStr "Please enter a password\n> "
     password <- getLine
-    print (checkPasswordLength password)
+    print (requireAlphaNum password)
+    -- print (checkPasswordLength password && requireAlphaNum password)
