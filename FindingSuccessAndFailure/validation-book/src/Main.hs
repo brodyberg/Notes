6ddba67ea541b6225e3bdabb6bbc47b577610118
Brody Berg <brodyberg@gmail.com>
2019-09-11 22:31:49 -0700

    CheckAllThree

--- FindingSuccessAndFailure/validation-book/src/Main.hs
+++ FindingSuccessAndFailure/validation-book/src/Main.hs
@@ -4,9 +4,9 @@ import Data.Char (isAlphaNum, isSpace)
 
 checkPasswordLength :: String -> Maybe String
 checkPasswordLength password = 
-  case (length password < 20) of
-    True -> Just password
-    _ -> Nothing
+  case (length password > 20 || length password < 4) of
+    True -> Nothing
+    _ -> Just password
 
 requireAlphaNum :: String -> Maybe String
 requireAlphaNum xs = 
@@ -20,6 +20,18 @@ cleanWhitespace (x:xs) =
   case (isSpace x) of 
     True -> cleanWhitespace xs
     False -> Just (x:xs)
+
+checkAllThree :: String -> Maybe String
+checkAllThree password = 
+  case cleanWhitespace password of 
+    Just cleaned -> 
+      case requireAlphaNum cleaned of
+        Just cleanedAlpha -> 
+          case checkPasswordLength cleanedAlpha of
+            Just cleanAlphaSized -> Just cleanAlphaSized
+            _ -> Nothing
+        _ -> Nothing
+    _ -> Nothing
 
 main :: IO ()
 main = 
