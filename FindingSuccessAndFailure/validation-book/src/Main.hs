b806b8cb979ca8becc6fc8a0df12fef3d1f02667
Brody Berg <brodyberg@gmail.com>
2019-09-21 12:41:01 -0700

    Cleanup

--- FindingSuccessAndFailure/validation-book/src/Main.hs
+++ FindingSuccessAndFailure/validation-book/src/Main.hs
@@ -90,23 +90,5 @@ test = printTestResult $
    
 main :: IO ()
 main = 
-  -- putStr "Please enter a username\n> " >> getLine >>= (\l -> print $ validateUsername (Username l)) 
-  -- >> putStr "Please enter a password\n> " >> getLine >>= (\l -> print $ validatePassword (Password l))
-
   putStr "Please enter a username\n> " >> getLine >>= print . validateUsername . Username >>
   putStr "Please enter a password\n> " >> getLine >>= print . validatePassword . Password
-
-
-  -- putStr "Please enter a username\n> " >> 
-  -- getLine >>= (print $ validateUsername . Username) 
-
--- main :: IO ()
--- main = 
---   do
---     putStr "Please enter a username\n> "
---     username <- Username <$> getLine
---     print (validateUsername username)
-
---     putStr "Please enter a password\n> "
---     password <- Password <$> getLine
---     print (validatePassword password)
