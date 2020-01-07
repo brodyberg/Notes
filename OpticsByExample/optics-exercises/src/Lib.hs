f873238e05b9ab939eabd0899e08fc894646863a
Brody Berg <brodyb@microsoft.com>
2020-01-07 07:32:32 -0800

    Lensy extensions and imports

--- OpticsByExample/optics-exercises/src/Lib.hs
+++ OpticsByExample/optics-exercises/src/Lib.hs
@@ -2,5 +2,20 @@ module Lib
     ( someFunc
     ) where
 
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Control.Lens
+import Control.Applicative
+import Data.Char
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
