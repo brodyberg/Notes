0ef6c0e33644b083275e56e8af9803934a364b83
Brody Berg <brodyb@microsoft.com>
2020-01-21 16:53:46 -0800

    First lens

--- OpticsByExample/optics-exercises/src/Lib.hs
+++ OpticsByExample/optics-exercises/src/Lib.hs
@@ -1,5 +1,8 @@
 module Lib
     ( someFunc
+    , numCrew
+    , name
+    , purplePearl
     ) where
 
 {-# LANGUAGE TemplateHaskell #-}
@@ -16,6 +19,36 @@ import Data.Char
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
+
+data Ship =
+    Ship { _name    :: String
+         , _numCrew :: Int
+         }
+    deriving(Show)
+
+getNumCrew :: Ship -> Int
+getNumCrew = _numCrew
+
+setNumCrew :: Ship -> Int -> Ship
+setNumCrew ship newNumCrew = ship { _numCrew = newNumCrew }
+
+numCrew :: Lens' Ship Int
+numCrew = lens getNumCrew setNumCrew
+
+getName :: Ship -> String
+getName = _name
+
+setName :: Ship -> String -> Ship
+setName ship newName = ship { _name = newName }
+
+name :: Lens' Ship String
+name = lens getName setName
+
+purplePearl :: Ship
+purplePearl =
+    Ship { _name    = "Purple Pearl"
+         , _numCrew = 74
+         }
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
