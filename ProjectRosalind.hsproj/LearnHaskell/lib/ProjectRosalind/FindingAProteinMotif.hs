4a814bcd6ba1202fbd64e307f11b5b6fa38fb14d
Brody Berg <brodyberg@gmail.com>
2021-06-04 21:47:29 -0700

    First ever Haskell https call!
    Figured out how to make an https call which will be required for the next Project Rosalind problem which involves pulling fasta strings from a website.
    
    This was an accomplishment because I couldn't use Network.HTTP.Client.Simple and had to figure out how to pass in the TLS manager myself.

---
+++ ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/FindingAProteinMotif.hs
@@ -0,0 +1,12 @@
+module ProjectRosalind.FindingAProteinMotif where
+  
+-- make a dfa that recognizes a DSL for motifs
+-- we could easily take the Fasta lib and learn from 
+-- it to author a parser for this and then run it
+-- on the output of http requests given the 
+-- UniProt Protein Database access IDs.
+
+-- https://www.uniprot.org/uniprot/B5ZC00.fasta
+-- https://www.uniprot.org/uniprot/A2Z669.fasta
+-- https://www.uniprot.org/uniprot/P07204.fasta
+-- 
