ce7e098a7baa717cb660f07e96c59e9a36644c3c
Brody Berg <brodyberg@gmail.com>
2021-06-17 21:19:19 -0700

    Work on my performance problem
    Summary: recursion to find all substrings is extremely slow. We are talking like maybe several 10s of minutes to compute all substrings for one fasta sequence.

--- ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/FindingASharedMotif.hs
+++ ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/FindingASharedMotif.hs
@@ -1,7 +1,7 @@
 module ProjectRosalind.FindingASharedMotif where
 
 import Test.QuickCheck
-import Data.List (intersect, maximumBy)
+import Data.List (intersect, maximumBy, take)
 import Data.Function (on)
 
 import ProjectRosalind.Fasta (parseFasta')
@@ -9,12 +9,29 @@ import ProjectRosalind.Fasta_Types
 import System.IO  
 import Control.Monad
 
+import Text.Regex.TDFA ((=~))
+
+import Data.Time
+
+-- speed ideas: 
+-- do the usual common substring approach for the first n
+-- but then, convert over to a simple search using those
+
+-- take first
+-- make all common substrings
+-- continually search each for the previously matched
+--   substrings
+-- but after the first string, you don't make all common substrings
+--   again
+
 allSubstrings :: String -> [String]
 allSubstrings [] = []
 allSubstrings strings = allSubstrings' strings []
   where 
     allSubstrings' [] acc = acc
     allSubstrings' str acc = (substrings str) ++ (allSubstrings' (tail str) [])
+
+-- do everything here: https://wiki.haskell.org/Dynamic_programming_example
 
 substrings :: String -> [String]
 substrings []   = []
@@ -29,8 +46,12 @@ getLongestSubstring :: [String] -> String
 getLongestSubstring strings = 
   maximumBy (compare `on` length) resultsOfIntersections
   where 
+    resultsAsListOfLists :: [[String]]
     resultsAsListOfLists = fmap allSubstrings strings
+
     resultsOfIntersections = foldr intersect (head resultsAsListOfLists) (tail resultsAsListOfLists)
+
+-- longest run: 5:20
 
 -- Formula from
 -- https://stackoverflow.com/questions/12418590/finding-substrings-of-a-string
@@ -39,7 +60,7 @@ prop_allPossibleSubstringCount str =
   length (allSubstrings str) == (n * (n + 1)) `div` 2
   where n = length str
 
-fileName = "/Users/brodyberg/Documents/GitHub/Notes/ProjectRosalind.hsproj/LearnHaskell/FindingASharedMotif/rosalind_lcsm_1.txt"
+fileName = "/Users/brodyberg/Documents/GitHub/Notes/ProjectRosalind.hsproj/LearnHaskell/FindingASharedMotif/rosalind_lcsm_2.txt"
 
 readLocalFile :: String -> IO String
 readLocalFile path = do  
@@ -54,8 +75,91 @@ filePathToFastas path = do
   
 mainSubstrings :: IO ()
 mainSubstrings = do
+    now <- getZonedTime  
+
+    putStrLn "START: prestart " 
+    putStrLn $ show now
+
     fastas <- filePathToFastas fileName
 
-    let longest = getLongestSubstring $ fmap fastaSeq fastas
+    now <- getZonedTime  
+    putStrLn "START: all substrings on one"
+    putStrLn $ show now
 
-    putStrLn longest
+    let first = fastas !! 0
+    let dna = fastaSeq first
+
+    let allSubs = allSubstrings dna
+    
+    putStrLn $ show allSubs
+
+    now <- getZonedTime  
+    putStrLn "END: all substrings on one"
+    putStrLn $ show now
+    
+    putStrLn "START: Making list of all fastas" 
+    now <- getZonedTime
+    putStrLn $ show now
+   
+    let fastaList = fmap fastaSeq fastas
+    now <- getZonedTime
+    putStrLn "DONE: Making list of all fastas" 
+    putStrLn $ show now
+
+    now <- getZonedTime
+    putStrLn "START: allSubstrings on all fastas" 
+    putStrLn $ show now
+
+    let resultsAsListOfLists = fmap allSubstrings fastaList
+
+    now <- getZonedTime
+    putStrLn "END: allSubstrings on all fastas" 
+    putStrLn $ show now
+
+   
+    let no = foldr intersect (head resultsAsListOfLists) []
+    
+    putStrLn "foo"
+    
+    let shortList = take 2 resultsAsListOfLists
+
+    let nope = foldr intersect (head shortList) (tail shortList)
+
+    putStrLn $ show nope
+    
+    let bar = maximumBy (compare `on` length) nope
+   
+    putStrLn "bar"
+
+    -- ok, so this forced computation that wasn't otherwise happening
+    putStrLn bar
+    
+    -- put all substrings into a dictionary where the substring is the key
+    -- but what's the value? how to check it's the substring of all fasta?
+ 
+--    putStrLn "START: getLongestSubstring 2 fastas" 
+--    now <- getZonedTime
+--    putStrLn $ show now
+--   
+--    let longest = getLongestSubstring fastaList
+--
+--    putStrLn longest
+--
+--    now <- getZonedTime
+--    putStrLn "END: getLongestSubstring 2 fastas" 
+--    putStrLn $ show now
+
+    putStrLn "Done"
+
+    
+--
+--    let longest = getLongestSubstring $ fmap fastaSeq fastas
+--
+--    putStrLn longest
+--
+--    end <- getZonedTime
+--
+--    putStrLn "end: "     
+--    putStrLn $ show end
+    
+
