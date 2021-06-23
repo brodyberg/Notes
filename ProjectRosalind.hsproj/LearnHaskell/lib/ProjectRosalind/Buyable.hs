afb7d9dd5409abb62a059df06f006cbe13320154
Brody Berg <brodyberg@gmail.com>
2021-06-22 18:58:18 -0700

    Solution in terms of correctness

--- ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
+++ ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
@@ -1,18 +1,10 @@
 module ProjectRosalind.Buyable where
+
+import Test.QuickCheck
   
 import Data.Vector as V
 
---import Data.Array((!), listArray)
---import qualified Data.Array as A ((!), listArray)
 
---a = listArray (0,1) ["Hello", "World"]
-
---buyable :: Int -> Bool
---buyable n = r!n
---  where
---    r = listArray (0,n) (map f [0..n])
---    f i = i == 0 || i >= 6 && r!(i-6) || i >= 9 && r!(i-9) || i >= 20 && r!(i-20)
-    
 subCount n = (n * (n + 1)) `div` 2
 
 x = "MSRVGKYPVEVPAGVQVSVADGFFKAKGKLGELTVPVSRHVEVKIEGSNVSVAPVGRRS"
@@ -20,33 +12,12 @@ x = "MSRVGKYPVEVPAGVQVSVADGFFKAKGKLGELTVPVSRHVEVKIEGSNVSVAPVGRRS"
 y = "GATTACA"
 y' = subCount $ Prelude.length y
 
---x = fmap (y !!) [0..3]
---x
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Formula from
+-- https://stackoverflow.com/questions/12418590/finding-substrings-of-a-string
+prop_buildCount :: String -> Bool
+prop_buildCount str = 
+  Prelude.length (build str) == (n * (n + 1)) `div` 2
+  where n = Prelude.length str
 
 -- Carry string 
 -- Copy of carry (block)
@@ -72,7 +43,6 @@ build str = create (V.fromList str) (V.fromList str) []
       --   3. add nothing to block
       else if blockLen == 0 
       then 
---        create (V.tail carry) (V.tail carry) ((V.fromList ">") : acc)
         create (V.tail carry) (V.tail carry) acc
       -- There is more carry
       -- There is more of this block: 
@@ -80,7 +50,6 @@ build str = create (V.fromList str) (V.fromList str) []
       --   2. bump one off tail of block
       --   3. save block to acc
       else
---        create carry (V.init block) ((V.fromList "^") : (block : acc))
         create carry (V.init block) (block : acc)
         
       where 
@@ -90,84 +59,7 @@ build str = create (V.fromList str) (V.fromList str) []
         blockLen :: Int
         blockLen = V.length block
 
-
-      -- we never march the HEAD of carry forward
-
-
-
-
-      -- missing case is: more of block no more carry      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-maker :: String -> [String]
-maker str = construct len 0 (len - 1) [str] 1 0
-  where 
-    len = Prelude.length str    
-
-               --length l      r      results     ix     vlook
-    construct :: Int -> Int -> Int -> [String] -> Int -> Int -> [String]
-    construct len l r results ix vlook = 
-      if l == (len - 1)
-      then results
-      else if (l == r)
-        then construct len (l + 1) (len - 1) results' (ix + 1) vlook
-      else
-        construct len l (r - 1) results' (ix + 1) (vlook + 1)
-        
-      -- we need to use vlook to pull back a string to build from here
-      -- we need to zero it out when we do that
-
-      where 
-        
-        -- needs to knock one off the back
-        results' :: [String]
-        results' = (results !! (ix - 1)) : results 
-        
-        substring :: Int -> Int -> String -> String
-        substring l r s = result
-
-          where 
-            (result, _, _, _) = Prelude.foldr slice ("", l, r, 0) s            
-
-            slice :: Char -> (String, Int, Int, Int) -> (String, Int, Int, Int)
-            slice item (subStr, l, r, ix) = 
-              if (ix >= l && ix <= r) 
-              then (item : subStr, l, r, ix + 1) 
-              else (subStr, l, r, ix + 1)
-
---foldr (\item (subStr, l, r, ix) -> if (ix >= l && ix <= r) then (item : subStr, l, r, ix + 1)  else (subStr, l, r, ix + 1)) ("", 2, 4, 0) y
-
-
-
--- WHEN WE INCREMENT L THE COUNT OF LAST PRODUCED
--- SUBSTRINGS IS OUR LOOKBACK TO GET THE FOUNDATION 
--- STRING FOR NEXT ROUND
+-- Image of what build does: 
 
 -- GATTACA
 -- GATTAC
@@ -198,56 +90,3 @@ maker str = construct len 0 (len - 1) [str] 1 0
 --      C
 --       A
  
-
-
--- f[0] = 0
--- f[1] = 1
--- for (i = 2; i <= n; i++) {
---   f[i] = f[i-1] + f[i - 2];
--- }
-
--- Start left at 0
--- Right at length - 1
---
--- Count down right pointer
--- When right pointer “touches” left, increment left by one, reset right to length -1
--- Nested comprehensions?
-
-
---ss :: String -> [String] 
----- GHC.Arr.Array Int String -- [String] -- Array Int String
---ss string = lst
---  where
---    l = (n * (n + 1)) `div` 2
---    n = length string
---    lst = listArray (0,l) (map f [0..l])
---
---    f :: Int -> String
---    f 0 = show $ head string
---    f i = ""
-
---allSubstrings :: String -> [String]
---allSubstrings [] = []
---allSubstrings strings = allSubstrings' strings []
---  where 
---    allSubstrings' [] acc = acc
---    allSubstrings' str acc = (substrings str) ++ (allSubstrings' (tail str) [])
---
----- do everything here: https://wiki.haskell.org/Dynamic_programming_example
---
---substrings :: String -> [String]
---substrings []   = []
---substrings item = ss "" item []
---  where 
---    ss [] [] acc = acc
---    ss [] therest acc = ss [(head therest)] (tail therest) acc
---    ss save [] acc = save : acc 
---    ss save therest acc = ss (save ++ [(head therest)]) (tail therest) (save : acc)
---
---
----- Formula from
----- https://stackoverflow.com/questions/12418590/finding-substrings-of-a-string
---prop_allPossibleSubstringCount :: String -> Bool
---prop_allPossibleSubstringCount str = 
---  length (allSubstrings str) == (n * (n + 1)) `div` 2
---  where n = length str
