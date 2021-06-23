2cd8769798d8f91ad4489805a399a26cd4536520
Brody Berg <brodyberg@gmail.com>
2021-06-22 20:59:41 -0700

    Start using Set (Vector Char)
    If we use Set we can avoid O(n) operations of Data.List. We also get a datastructure which filters duplicates automatically. Drawback is that we have to pass around "block" again since we can't look at the item previously inserted into our "acc" Vector Char since the acc is now Set (Vector Char).

--- ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
+++ ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
@@ -1,7 +1,8 @@
 module ProjectRosalind.Buyable where
 
 import Test.QuickCheck
-  
+
+import Data.Set as S
 import Data.Vector as V
 
 
@@ -19,45 +20,137 @@ prop_buildCount str =
   Prelude.length (build str) == (n * (n + 1)) `div` 2
   where n = Prelude.length str
 
--- Carry string 
--- Copy of carry (block)
--- Next is that without last item
--- Continue while there’s any more of copy 
--- None left? Knock first from carry, loop
 
-build :: String -> [String]
-build str = create (V.fromList str) (V.fromList str) []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+build :: String -> Set (V.Vector Char)
+build str = create (V.fromList str) (V.fromList str) S.empty
   where 
-    create :: (V.Vector Char) -> (V.Vector Char) -> [V.Vector Char] -> [String]
+    create :: (Vector Char) -> (Vector Char) -> Set (Vector Char) -> Set (Vector Char)
     create carry block acc = 
       -- No more carry
       -- No more block:
       --   1. return acc
       if carryLen == 0 && blockLen == 0
       then
-        fmap (V.toList) acc
+        acc
       -- There is more carry
       -- No more of this block:
       --   1. bump one off tail of carry
-      --   2. copy that to block
-      --   3. add nothing to block
-      else if blockLen == 0 
+      --   2. new carry value placed as new block
+      else if blockLen == 0
       then 
-        create (V.tail carry) (V.tail carry) acc
+        create (V.tail carry) (V.tail carry) (S.insert (V.tail carry) acc)
       -- There is more carry
       -- There is more of this block: 
       --   1. do not touch carry
       --   2. bump one off tail of block
       --   3. save block to acc
       else
-        create carry (V.init block) (block : acc)
+        create carry (V.init block) (S.insert (V.init block) acc)
         
       where 
         carryLen :: Int
         carryLen = V.length carry
-
+        
         blockLen :: Int
         blockLen = V.length block
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Carry string 
+-- Copy of carry (block)
+-- Next is that without last item
+-- Continue while there’s any more of copy 
+-- None left? Knock first from carry, loop
+
+--build :: String -> [String]
+--build str = create (V.fromList str) (V.fromList str) []
+--  where 
+--    create :: (V.Vector Char) -> (V.Vector Char) -> [V.Vector Char] -> [String]
+--    create carry block acc = 
+--      -- No more carry
+--      -- No more block:
+--      --   1. return acc
+--      if carryLen == 0 && blockLen == 0
+--      then
+--        fmap (V.toList) acc
+--      -- There is more carry
+--      -- No more of this block:
+--      --   1. bump one off tail of carry
+--      --   2. copy that to block
+--      --   3. add nothing to block
+--      else if blockLen == 0 
+--      then 
+--        create (V.tail carry) (V.tail carry) acc
+--      -- There is more carry
+--      -- There is more of this block: 
+--      --   1. do not touch carry
+--      --   2. bump one off tail of block
+--      --   3. save block to acc
+--      else
+--        create carry (V.init block) (block : acc)
+--        
+--      where 
+--        carryLen :: Int
+--        carryLen = V.length carry
+--
+--        blockLen :: Int
+--        blockLen = V.length block
 
 -- Image of what build does: 
 
