8d650045233d9a502303411fdbe1b386f4df0d4d
Brody Berg <brodyberg@gmail.com>
2021-06-24 20:30:53 -0700

    List comprehension slicing breakthrough
    For the first time I've created a list comprehension which appears to be close to what I'm going for: a list of the endpoints of all possible substrings I can use in the following linear algorithm:
    
    z = concat [ [ (l,r) | l <- [0..(length y)], l < r ] | r <- [(length y),((length y) - 1)..1] ]
    z
    length z
    -- now, given
    -- a map from 0 to (n * (n + 1)) `div` 2
    -- where n is length str
    -- we can index into z to get slices
    -- for all possible substrings of str
    -- linearly with no recursion

--- ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
+++ ProjectRosalind.hsproj/LearnHaskell/lib/ProjectRosalind/Buyable.hs
@@ -1,196 +1,16 @@
 module ProjectRosalind.Buyable where
-
-import Test.QuickCheck
-
-import Data.Set as S
-import Data.Vector as V
-
+--
+--import Test.QuickCheck
+--
+--import Data.Set as S
+--import Data.Vector as V
+--
+--
 
 subCount n = (n * (n + 1)) `div` 2
 
-x = "MSRVGKYPVEVPAGVQVSVADGFFKAKGKLGELTVPVSRHVEVKIEGSNVSVAPVGRRS"
-
-y = "GATTACA"
-y' = subCount $ Prelude.length y
-
--- Formula from
--- https://stackoverflow.com/questions/12418590/finding-substrings-of-a-string
-prop_buildCount :: String -> Bool
-prop_buildCount str = 
-  Prelude.length (build str) == (n * (n + 1)) `div` 2
-  where n = Prelude.length str
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- l = 0, count down from length to l + 1
--- increment l by one, count down from length to l+1
-
--- do all our dna strings have exactly the same length?
--- if they do, we could calculate the cuts once
-
--- indeed, they are all exactly 1000 characters long: 
---[1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000]
-
--- so, if we calculate the slices once
--- and then slice dna-count times 
--- how fast are we?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-build :: String -> Set (V.Vector Char)
-build str = create (V.fromList str) (V.fromList str) S.empty
-  where 
-    create :: (Vector Char) -> (Vector Char) -> Set (Vector Char) -> Set (Vector Char)
-    create carry block acc = 
-      -- No more carry
-      -- No more block:
-      --   1. return acc
-      if carryLen == 0 && blockLen == 0
-      then
-        acc
-      -- There is more carry
-      -- No more of this block:
-      --   1. bump one off head of carry
-      --   2. new carry value placed as new block
-      --   3. insert that carry value into acc
-      else if blockLen == 0
-      then 
-        create (V.tail carry) (V.tail carry) acc
-      -- There is more carry
-      -- There is more of this block: 
-      --   1. do not touch carry
-      --   2. bump one off tail of block
-      --   3. save block to acc
-      else
-        create carry (V.init block) (S.insert block acc)
-        
-      where 
-        carryLen :: Int
-        carryLen = V.length carry
-        
-        blockLen :: Int
-        blockLen = V.length block
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Carry string 
--- Copy of carry (block)
--- Next is that without last item
--- Continue while there’s any more of copy 
--- None left? Knock first from carry, loop
-
---build :: String -> [String]
---build str = create (V.fromList str) (V.fromList str) []
---  where 
---    create :: (V.Vector Char) -> (V.Vector Char) -> [V.Vector Char] -> [String]
---    create carry block acc = 
---      -- No more carry
---      -- No more block:
---      --   1. return acc
---      if carryLen == 0 && blockLen == 0
---      then
---        fmap (V.toList) acc
---      -- There is more carry
---      -- No more of this block:
---      --   1. bump one off tail of carry
---      --   2. copy that to block
---      --   3. add nothing to block
---      else if blockLen == 0 
---      then 
---        create (V.tail carry) (V.tail carry) acc
---      -- There is more carry
---      -- There is more of this block: 
---      --   1. do not touch carry
---      --   2. bump one off tail of block
---      --   3. save block to acc
---      else
---        create carry (V.init block) (block : acc)
---        
---      where 
---        carryLen :: Int
---        carryLen = V.length carry
---
---        blockLen :: Int
---        blockLen = V.length block
-
 -- Image of what build does: 
-
+   0     6
 -- GATTACA
 -- GATTAC
 -- GATTA
@@ -220,3 +40,220 @@ build str = create (V.fromList str) (V.fromList str) S.empty
 --      C
 --       A
  
+
+
+
+--subCount n = (n * (n + 1)) `div` 2
+--
+--x = "MSRVGKYPVEVPAGVQVSVADGFFKAKGKLGELTVPVSRHVEVKIEGSNVSVAPVGRRS"
+--
+--y = "GATTACA"
+--y' = subCount $ Prelude.length y
+--
+---- Formula from
+---- https://stackoverflow.com/questions/12418590/finding-substrings-of-a-string
+--prop_buildCount :: String -> Bool
+--prop_buildCount str = 
+--  Prelude.length (build str) == (n * (n + 1)) `div` 2
+--  where n = Prelude.length str
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+---- l = 0, count down from length to l + 1
+---- increment l by one, count down from length to l+1
+--
+---- do all our dna strings have exactly the same length?
+---- if they do, we could calculate the cuts once
+--
+---- indeed, they are all exactly 1000 characters long: 
+----[1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000]
+--
+---- so, if we calculate the slices once
+---- and then slice dna-count times 
+---- how fast are we?
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--build :: String -> Set (V.Vector Char)
+--build str = create (V.fromList str) (V.fromList str) S.empty
+--  where 
+--    create :: (Vector Char) -> (Vector Char) -> Set (Vector Char) -> Set (Vector Char)
+--    create carry block acc = 
+--      -- No more carry
+--      -- No more block:
+--      --   1. return acc
+--      if carryLen == 0 && blockLen == 0
+--      then
+--        acc
+--      -- There is more carry
+--      -- No more of this block:
+--      --   1. bump one off head of carry
+--      --   2. new carry value placed as new block
+--      --   3. insert that carry value into acc
+--      else if blockLen == 0
+--      then 
+--        create (V.tail carry) (V.tail carry) acc
+--      -- There is more carry
+--      -- There is more of this block: 
+--      --   1. do not touch carry
+--      --   2. bump one off tail of block
+--      --   3. save block to acc
+--      else
+--        create carry (V.init block) (S.insert block acc)
+--        
+--      where 
+--        carryLen :: Int
+--        carryLen = V.length carry
+--        
+--        blockLen :: Int
+--        blockLen = V.length block
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+---- Carry string 
+---- Copy of carry (block)
+---- Next is that without last item
+---- Continue while there’s any more of copy 
+---- None left? Knock first from carry, loop
+--
+----build :: String -> [String]
+----build str = create (V.fromList str) (V.fromList str) []
+----  where 
+----    create :: (V.Vector Char) -> (V.Vector Char) -> [V.Vector Char] -> [String]
+----    create carry block acc = 
+----      -- No more carry
+----      -- No more block:
+----      --   1. return acc
+----      if carryLen == 0 && blockLen == 0
+----      then
+----        fmap (V.toList) acc
+----      -- There is more carry
+----      -- No more of this block:
+----      --   1. bump one off tail of carry
+----      --   2. copy that to block
+----      --   3. add nothing to block
+----      else if blockLen == 0 
+----      then 
+----        create (V.tail carry) (V.tail carry) acc
+----      -- There is more carry
+----      -- There is more of this block: 
+----      --   1. do not touch carry
+----      --   2. bump one off tail of block
+----      --   3. save block to acc
+----      else
+----        create carry (V.init block) (block : acc)
+----        
+----      where 
+----        carryLen :: Int
+----        carryLen = V.length carry
+----
+----        blockLen :: Int
+----        blockLen = V.length block
+--
+---- Image of what build does: 
+--
+---- GATTACA
+---- GATTAC
+---- GATTA
+---- GATT
+---- GAT
+---- GA
+---- G
+----  ATTACA
+----  ATTAC
+----  ATTA
+----  ATT
+----  AT
+----  A
+----   TTACA
+----   TTAC
+----   TTA
+----   TT
+----   T
+----    TACA
+----    TAC
+----    TA
+----    T
+----     ACA
+----     AC
+----     A
+----      CA
+----      C
+----       A
+-- 
